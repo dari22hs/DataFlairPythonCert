46d92147c72d78a9fb35605c1367d1a3179c18fb
Dari Huallpa <dariojoel152@gmail.com>
2023-09-03 20:21:30 -0600

    - Add tuples file

--- tuples.py
+++ tuples.py
@@ -20,5 +20,44 @@ Collection of items that may be different, tuples are immutable and ordered.
 # print(b) # Output is 97
 
 ## Accessing to elements
-myTuple = (1, 2, 3, 4, 5)
-print(myTuple[3])
+# myTuple = (1, 2, 3, 4, 5)
+# print(myTuple[3])
+
+## Slicing tuples
+# percentages = (99, 97, 95, 92, 90, 98)
+# print(percentages[2:4]) # Output (95, 92)
+# print(percentages[:4]) # Output (99, 97, 95, 92)
+# print(percentages[4:]) # Output (90, 98)
+# print(percentages[2:2]) # Output ()
+
+# print(percentages[:-2]) # Output (99, 97, 95, 92)
+# print(percentages[-2:]) # Output (90, 98)
+# print(percentages[2:-2]) # Output (95, 92)
+# print(percentages[:]) # Output (99, 97, 95, 92, 90, 98)
+# del percentages
+# print(percentages) # Output -> NameError: name 'percentages' is not defined
+
+## Reassigning tuples
+# myTuple = (1, 2, 3, [6, 5])
+# myTuple[3][0] = 4
+# print(myTuple)
+
+# myTuple2 = (1, 2, 300)
+# print(min(myTuple2))
+# print(max(myTuple2))
+# print(sum(myTuple2))
+
+# myTuple3 = ('hi', 'HI', 'hello')
+# print(len(myTuple3))
+# print(max(myTuple3))
+
+# myTuple4 = ("", "0", "")
+# myTuple5 = ("", 0, "")
+# print(any(myTuple4))
+# print(any(myTuple5))
+
+myTuple6 = ('1', 1, True, '')
+print(all(myTuple6))
+
+
+
